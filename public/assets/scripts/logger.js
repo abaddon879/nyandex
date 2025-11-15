50e06004c7d43f4a50b6db5f6ad2860ceb672d06
abaddon879 <davidwilson_86@hotmail.com>
2025-11-15 18:49:58 +0000

    Removing old files

--- public/assets/scripts/logger.js
+++
@@ -1,4 +0,0 @@
-export const Logger = {
-    info: (message, data = '') => { console.info(`[${new Date().toLocaleTimeString()}] [INFO] ${message}`, data); },
-    error: (message, error = '') => { console.error(`[${new Date().toLocaleTimeString()}] [ERROR] ${message}`, error); }
-};
--- views/register-success.php
+++
@@ -1,3 +0,0 @@
-<h1>Register</h1>
-
-<p>Thank you for signing up. You can now <a href="/login">log in</a>.</p>
